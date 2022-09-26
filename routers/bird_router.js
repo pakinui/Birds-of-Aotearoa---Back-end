db1c14c510f7813418140f033147c448450e0233
schpo592 <schpo592@student.otago.ac.nz>
2022-09-26 21:39:44 +1300

    edit partially working

--- routers/bird_router.js
+++ routers/bird_router.js
@@ -33,7 +33,7 @@ const exp = require('express-fileupload');
 // TODO: finishe the "Create" route(s)
 router.get('/create', async (req, res) => {
     console.log('create');
-    
+
     // console.log(req);
     // currently does nothing except redirect to home page
     res.render('createPage');
@@ -48,34 +48,9 @@ router.use(
 router.post('/create', async (req, res) => {
     console.log('post create');
     console.log(req.body);
-    console.log(req.body.pName);
-    console.log(req.files.myFile);
-    // req.file.filename = originalname;
-    
-    // console.log(req.file);
-    // var tempPath = req.file.path;
-    // console.log(tempPath);
-    // var targetPath = req.file.originalname;
-    // console.log(targetPath);
+    //console.log(req.body.pName);
+    //console.log(req.files.myFile);
 
-   // var current = fs.createReadStream();
-    // fs.readFile(tempPath,  function (err, data) {
-    //     if(err){
-    //         console.log('ERORRRR');
-    //         throw err; //fail if it cant be read
-    //     } 
-    //     // fs.writeFile(targetPath, data, function (err) {
-    //     //     if(err){
-    //     //         console.log(err);
-    //     //         console.log('image upload error');
-    //     //     }else{
-    //     //         console.log('image upload worked');
-    //     //         res.render('/');
-    //     //     }
-            
-    //     // })
-    //     console.log('read file complete');
-    // });
     if (!req.files) {
         return res.status(400).send("No files were uploaded.");
     }
@@ -83,16 +58,16 @@ router.post('/create', async (req, res) => {
     const fileImg = req.files.myFile
     const fromPath = path.join(__dirname, "../public/images/", fileImg.name);
     console.log(`path: ${fromPath}`);
-    
-    fileImg.mv(fromPath, function(err) {
-        if(err){
+
+    fileImg.mv(fromPath, function (err) {
+        if (err) {
             console.log('file error');
             return res.status(500).send(err);
-        }else{
+        } else {
             console.log('file uploaded');
         }
-    } );
-    
+    });
+
     // const copyFile = util.promisify(fs.copyFile);
     // const img = req.file;
     // const results = Object.keys(img).map((key) => {
@@ -133,7 +108,7 @@ router.post('/create', async (req, res) => {
     //console.log(bird_info, '/birds/create response');
     //res.send('success!');
 
-   
+
 
 
 
@@ -169,26 +144,91 @@ router.get('/', async (req, res) => {
 
     console.log('home');
     //var birds =await  bird_controller.filter_bird_data(search, status, sort);
+    
     res.render('home', {
         birds: await bird_controller.filter_bird_data(search, status, sort)
+        
     });
 
 })
 
 // TODO: Update bird route(s)
-// router.get('/edit', async (req, res) => {
+router.get('/edit', async (req, res) => {
+    console.log('edit');
+    bird = await BirdModel.findOne({ _id: req.query.name });
+    console.log(`request id: ${bird._id}\n`);
+    console.log(`request id2: ${req.query.name}\n`);
+    res.render('editPage', {
+        birds: bird
+    });
+    // res.render('editPage');
+});
 
-// });
+router.post('/edit', async (req, res) => {
 
+    bird = await BirdModel.findOne({scientific_name: req.body.sName});
+    
+    console.log(`id?1?: ${bird}`);
+    console.log(`id?asd1?: ${bird._id}`);
+
+    const newBird = {
+        primary_name: req.body.pName,
+        english_name: req.body.eName,
+        scientific_name: req.body.sName,
+        order: req.body.ord,
+        family: req.body.fam,
+        status: req.body.cStatus,
+        size: {
+            height: {
+                value: req.body.height,
+                units: "g"
+            },
+            weight: {
+                value: req.body.weight,
+                units: "cm"
+            }
+        },
+        other_names: req.body.other_names
+        
+
+    };
+    if (!req.files) {
+        // return res.status(400).send("No files were uploaded.");
+        console.log('bird file not updated');
+        // const fileImg = 
+    } else {
+        console.log('file updated');
+        const fileImg = req.files.myFile
+        const fromPath = path.join(__dirname, "../public/images/", fileImg.name);
+        console.log(`path: ${fromPath}`);
+        fileImg.mv(fromPath, function (err) {
+            if (err) {
+                console.log('file error');
+                return res.status(500).send(err);
+            } else {
+                console.log('file uploaded');
+            }
+        });
+        newBird.photo.source=fileImg.name;
+    }
+    
+
+    console.log(newBird);
+    const bird_info = await BirdModel.findByIdAndUpdate(bird._id, newBird);
+    console.log(`info: ${bird._id}`);
+    
+
+    res.redirect(`/birds/bird/?name=${newBird.scientific_name}`);
+});
 // TODO: Delete bird route(s)
 router.get('/delete', async (req, res) => {
-     console.log(req.query.name);
-    var bird = await BirdModel.findOne({scienfitic_name: req.query.name});
+    console.log(req.query.name);
+    var bird = await BirdModel.findOne({ scienfitic_name: req.query.name });
     console.log(bird._id.toString());
     // const idd = req.id;
     // console.log(bird);
-    
-    const bd_info = await BirdModel.findOneAndDelete({scientific_name: req.query.name});
+
+    const bd_info = await BirdModel.findOneAndDelete({ scientific_name: req.query.name });
     console.log(bd_info, 'birds/delete response');
 
     res.redirect('/');
